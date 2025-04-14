524515caeff8a86c1e8bb76381045ee372ad654c
TimGorter <34648298+bobgithub12@users.noreply.github.com>
2025-04-14 16:27:51 +0200

    log working without tree

--- view/learning_path.py
+++ view/learning_path.py
@@ -1,35 +1,37 @@
 from ipywidgets import widgets
-from ipytree import Tree, Node
 
 class LearningPathView:
-    def __init__(self, controller, Tree, Node):
-        self.Node = Node
+    def __init__(self, controller):
         self.controller = controller
-        self.tree = Tree(stripes=True)
-        self.vbox =  widgets.VBox([self.tree])
-        # self.vbox =  widgets.VBox([])
+
+        self.list = widgets.SelectMultiple(
+            options=[],
+            value=[],
+            disabled=True,
+            rows=20,
+            layout={'width': '50%'}
+        )
+        self.vbox =  widgets.VBox([self.list])
 
     def get_icon(self, category):
         if category == 'SelectData':
-            return 'file'
+            return '(Data selection)'
         if category == 'DataCleaning':
-            return 'broom'
+            return '(Data cleaning)'
         if category == 'DataProcessing':
-            return 'repeat'
+            return '(Data processing)'
         if category == 'ModelDevelopment':
-            return 'cube'
+            return '(Model development)'
         return ''
 
     def update_actions(self):
         actions = self.controller.get_list_of_actions()
-
-        for node in self.tree.nodes:
-            self.tree.remove_node(node)
+        updated_action_array = []
 
         for action in actions:
-            node = self.Node(str(action[2]) + ': ' + action[1])
-            node.icon = self.get_icon(action[0])
-            self.tree.add_node(node)
+            updated_action_array += [str(action[2]) + ': ' + action[1]  + ' ' + self.get_icon(action[0])]
+
+        self.list.options = updated_action_array
 
 
     def get_learning_path_tab(self):

7b27ce69e48e01f7e49e242ea9b6073f56b8da2f
TimGorter <34648298+bobgithub12@users.noreply.github.com>
2025-05-30 13:57:46 +0200

    remove prints

--- model/learning_manager.py
+++ model/learning_manager.py
@@ -146,30 +146,21 @@ class LearningManagerModel:
     
     def calculate_competence_vector(self, performance, reference_task, date):
         try:
-            print(1)
             current_task = self.controller.convert_performance_to_task(performance, "", "")
             target_column = self.controller.get_target_task(current_task)
             dataset_name = current_task["dataset"].replace(".csv", "")
-            print(2)
+
             if reference_task == None:
                 reference_task = self.controller.get_reference_task(target_column, dataset_name)       
                 if not reference_task:
-                    print("return none")
                     # reference task not found
                     return None
-            print(3)
             valid_performance = self.validate_performance(reference_task, performance)
-            print(valid_performance)
             if valid_performance:
-                print(4)
                 if not date:
                     date = performance.performance['General']['Date'][0]
-                print(date)
                 overlap_score = self.get_overlap_scores(reference_task, performance) #for example: {data_cleaning: 0.3,...}
-                print(5)
                 competence_vector = self.get_competence_vector(overlap_score, reference_task['difficulty'], date)
-                print(6)
-                print(competence_vector)
                 return competence_vector
 
         except Exception as e:

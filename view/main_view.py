52aea5719ce696e41c48df0fc0290d69c3b391c8
TimGorter <34648298+bobgithub12@users.noreply.github.com>
2026-01-19 10:39:16 +0100

    Update main_view.py

--- view/main_view.py
+++ view/main_view.py
@@ -25,7 +25,7 @@ class MainView:
     def set_tabs(self,tab_1,tab_2,tab_3,tab_4, tab_5):
         self.tabs = [tab_1,tab_2,tab_3, tab_4, tab_5]
         tab_set = widgets.Tab(self.tabs)
-        tab_set.set_title(0, 'Data Selection')
+        tab_set.set_title(0, 'Data Information')
         tab_set.set_title(1, 'Data Cleaning')
         tab_set.set_title(2, 'Data Processing')
         tab_set.set_title(3, 'Predictive Modeling')
